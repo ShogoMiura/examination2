ac91228571c720f02ba532fb823cb8d473ed68f9
Shogo Miura <138654083+ShogoMiura@users.noreply.github.com>
2024-01-23 18:37:45 +0900

    rootエンドポイントへのアクセスのテストを追加。

---
+++ src/main/java/com/examination2/examination2/presentation/EmployeeController.java
@@ -0,0 +1,8 @@
+package com.examination2.examination2.presentation;
+
+public class EmployeeController {
+
+    public void getRoot() {
+
+    }
+}
---
+++ src/test/java/com/examination2/examination2/presentation/EmployeeControllerTest.java
@@ -0,0 +1,33 @@
+package com.examination2.examination2.presentation;
+
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
+
+
+@WebMvcTest
+class EmployeeControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        RestAssuredMockMvc.mockMvc(mockMvc);
+    }
+
+    @Test
+    void ルートURLへのアクセスを受け付ける() {
+        given()
+                .when()
+                .get("/")
+                .then()
+                .statusCode(200);
+    }
+
+}
