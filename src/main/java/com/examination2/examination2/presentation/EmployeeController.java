2216a6e534712562ca3c539fce8c2b3aa7f82186
Shogo Miura <138654083+ShogoMiura@users.noreply.github.com>
2024-01-23 18:38:57 +0900

    rootエンドポイントへのアクセスのテストを追加。

--- src/main/java/com/examination2/examination2/presentation/EmployeeController.java
+++ src/main/java/com/examination2/examination2/presentation/EmployeeController.java
@@ -1,7 +1,23 @@
 package com.examination2.examination2.presentation;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * HTTPリクエストを処理するコントローラークラスです。
+ */
+@RestController
+@RequestMapping("/")
 public class EmployeeController {
 
+    /**
+     * ルートエンドポイントへのHTTP GETリクエストを処理します。
+     */
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public void getRoot() {
 
     }
