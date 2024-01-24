c2b011f3068c5a32a40e918dcc57480df0113359
Shogo Miura <138654083+ShogoMiura@users.noreply.github.com>
2024-01-24 19:28:45 +0900

    Employeeクラス仮実装。EmployeeTestクラス実装。

---
+++ src/main/java/com/examination2/miura/domain/Employee.java
@@ -0,0 +1,4 @@
+package com.examination2.miura.domain;
+
+public record Employee(String id, String firstName, String lastName) {
+}
---
+++ src/test/java/com/examination2/miura/domain/EmployeeTest.java
@@ -0,0 +1,14 @@
+package com.examination2.miura.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+class EmployeeTest {
+
+  @Test
+  void 従業員IDがnullの場合に例外が発生する() {
+    // execute & assert
+      assertThatThrownBy(() -> new Employee(null, "Taro", "Tanaka"))
+              .isInstanceOf(RuntimeException.class);
+  }
+}
