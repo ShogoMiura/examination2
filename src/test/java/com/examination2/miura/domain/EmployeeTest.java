c7a54f5260155b33f1f2607626b581c9f15e0df5
Shogo Miura <138654083+ShogoMiura@users.noreply.github.com>
2024-01-24 20:02:06 +0900

    EmployeeTestクラスにおいて、従業員IDに関してのガード条件のテストを実装。

--- src/test/java/com/examination2/miura/domain/EmployeeTest.java
+++ src/test/java/com/examination2/miura/domain/EmployeeTest.java
@@ -12,4 +12,20 @@ class EmployeeTest {
               .isInstanceOf(IllegalArgumentException.class)
               .hasMessage("従業員IDがnullです。");
   }
+
+  @Test
+  void 従業員IDが数字以外の場合に例外が発生する() {
+    // execute & assert
+    assertThatThrownBy(() -> new Employee("a", "Taro", "Tanaka"))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("従業員IDが数字ではありません。");
+  }
+
+  @Test
+  void 従業員IDが11桁以上の場合に例外が発生する() {
+    // execute & assert
+    assertThatThrownBy(() -> new Employee("11111111111", "Taro", "Tanaka"))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("従業員IDが大きすぎます。");
+  }
 }
