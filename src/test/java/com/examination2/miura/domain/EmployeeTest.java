128eb876997034faaa45fd89410258de9f7f3c3f
Shogo Miura <138654083+ShogoMiura@users.noreply.github.com>
2024-01-24 21:25:00 +0900

    Employeeクラスにおいて、従業員の名前と姓のガード条件のテストを追加。

--- src/test/java/com/examination2/miura/domain/EmployeeTest.java
+++ src/test/java/com/examination2/miura/domain/EmployeeTest.java
@@ -6,6 +6,9 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class EmployeeTest {
+  private static final String TOO_LONG_NAME
+          = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+          "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
 
   @ParameterizedTest
   @CsvSource(textBlock = """
@@ -21,5 +24,37 @@ class EmployeeTest {
     assertThatThrownBy(() -> new Employee(value, "Taro", "Tanaka"))
             .isInstanceOf(IllegalArgumentException.class)
             .hasMessage(message);
+  }
+
+  @Test
+  void 従業員のfirstNameがnullの場合例外が発生する() {
+    // execute & assert
+    assertThatThrownBy(() -> new Employee("1", null, "Tanaka"))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("従業員の名前がnullです。");
+  }
+
+  @Test
+  void 従業員のfirstNameが100文字を超える場合例外が発生する() {
+    // execute & assert
+    assertThatThrownBy(() -> new Employee("1", TOO_LONG_NAME, "Tanaka"))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("従業員の名前が100文字より大きいですです。");
+  }
+
+  @Test
+  void 従業員のlastNameがnullの場合例外が発生する() {
+    // execute & assert
+    assertThatThrownBy(() -> new Employee("1", "Taro", null))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("従業員の姓がnullです。");
+  }
+
+  @Test
+  void 従業員のlastNameが100文字を超える場合例外が発生する() {
+    // execute & assert
+    assertThatThrownBy(() -> new Employee("1", "Taaro", TOO_LONG_NAME))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("従業員の姓が100文字より大きいです。");
   }
 }
