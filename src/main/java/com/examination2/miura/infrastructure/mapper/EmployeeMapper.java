4f78341afaaaa874ff7ab97d337fafe6d799ea08
Shogo Miura <138654083+ShogoMiura@users.noreply.github.com>
2024-01-25 18:44:09 +0900

    従業員の全件検索に関して、EmployeeMapperクラスのfindAllEmployeesメソッドを実装。 全件検索の結果、テーブルが空だった場合のテストを追加。

--- src/main/java/com/examination2/miura/infrastructure/mapper/EmployeeMapper.java
+++ src/main/java/com/examination2/miura/infrastructure/mapper/EmployeeMapper.java
@@ -1,12 +1,21 @@
 package com.examination2.miura.infrastructure.mapper;
 
 import com.examination2.miura.infrastructure.entity.EmployeeEntity;
+import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
-import java.util.List;
-
-
+/**
+ * 従業員情報をデータベースから取得するためのマッパーインターフェースです。
+ */
+@Mapper
 public interface EmployeeMapper {
+
+  /**
+   * 従業員テーブルから全ての従業員情報を取得します。
+   *
+   * @return 従業員テーブルから取得したEmployeeEntity オブジェクトのリスト
+   */
+  @Select("SELECT * FROM employees")
   List<EmployeeEntity> findAllEmployees();
 }
--- src/test/java/com/examination2/miura/infrastructure/mapper/EmployeeMapperTest.java
+++ src/test/java/com/examination2/miura/infrastructure/mapper/EmployeeMapperTest.java
@@ -4,9 +4,11 @@ package com.examination2.miura.infrastructure.mapper;
 import com.examination2.miura.infrastructure.entity.EmployeeEntity;
 import com.github.database.rider.core.api.configuration.DBUnit;
 import com.github.database.rider.core.api.dataset.DataSet;
+import com.github.database.rider.core.api.dataset.ExpectedDataSet;
 import com.github.database.rider.junit5.api.DBRider;
 import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,6 +17,7 @@ import com.github.database.rider.core.api.connection.ConnectionHolder;
 import java.sql.DriverManager;
 import java.util.List;
 
+import static java.util.Collections.emptyList;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
@@ -36,19 +39,37 @@ class EmployeeMapperTest {
     Flyway.configure().dataSource(DB_URL, DB_USER, DB_PASSWORD).load().migrate();
   }
 
-  @Test
-  @DataSet(value = "datasets/setup/employees.yml")
-  void 従業員の全件検索が正常に行える場合() {
-    // setup
-    List<EmployeeEntity> expected = List.of(
-            new EmployeeEntity("1", "Taro", "Yamada"),
-            new EmployeeEntity("2", "Jiro", "Yamada")
-    );
+  @Nested
+  class 全件検索 {
+    @Test
+    @DataSet(value = "datasets/setup/employees.yml")
+    @ExpectedDataSet(value = "datasets/expected/employees.yml")
+    void 従業員の全件検索が正常に行える場合() {
+      // setup
+      List<EmployeeEntity> expected = List.of(
+              new EmployeeEntity("1", "Taro", "Yamada"),
+              new EmployeeEntity("2", "Jiro", "Yamada")
+      );
 
-    // execute
-    List<EmployeeEntity> actual = sut.findAllEmployees();
+      // execute
+      List<EmployeeEntity> actual = sut.findAllEmployees();
 
-    // assert
-    assertThat(actual).isEqualTo(expected);
+      // assert
+      assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    @DataSet(value = "datasets/setup/empty-employees.yml")
+    @ExpectedDataSet(value = "datasets/expected/empty-employees.yml")
+    void 全件検索の結果従業員が存在しない場合() {
+      // setup
+      List<EmployeeEntity> expected = emptyList();
+
+      // execute
+      List<EmployeeEntity> actual = sut.findAllEmployees();
+
+      // assert
+      assertThat(actual).isEqualTo(expected);
+    }
   }
 }
