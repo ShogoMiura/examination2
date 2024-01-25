072f7c22e624832df494ebde8756091cb95e5566
Shogo Miura <138654083+ShogoMiura@users.noreply.github.com>
2024-01-25 18:49:02 +0900

    EmployeeRepositoryインターフェース実装。

---
+++ src/main/java/com/examination2/miura/domain/EmployeeRepository.java
@@ -0,0 +1,17 @@
+package com.examination2.miura.domain;
+
+import java.util.List;
+
+/**
+ * 従業員情報に関するクエリを実行するためのリポジトリインターフェースです。
+ * このインターフェースは、従業員に関するデータを取得するためのメソッドを提供します。
+ */
+public interface EmployeeRepository {
+
+  /**
+   * すべての従業員のリストを取得します。
+   *
+   * @return すべての従業員を表す Employee オブジェクトのリスト。
+   */
+  List<Employee> findAllEmployees();
+}
