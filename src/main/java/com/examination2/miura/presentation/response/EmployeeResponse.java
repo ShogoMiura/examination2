9250617bdf3d3da2f5c4c0d5c06fd07fe0ea05d8
Shogo Miura <138654083+ShogoMiura@users.noreply.github.com>
2024-01-29 04:14:09 +0900

    EmployeeResponse実装。

---
+++ src/main/java/com/examination2/miura/presentation/response/EmployeeResponse.java
@@ -0,0 +1,20 @@
+package com.examination2.miura.presentation.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * 従業員情報のレスポンスを表すクラスです。
+ *
+ * @param id 従業員のID。
+ * @param firstName 従業員の名前。
+ * @param lastName 従業員の姓。
+ */
+public record EmployeeResponse(
+        @JsonProperty("id")
+        String id,
+        @JsonProperty("firstName")
+        String firstName,
+        @JsonProperty("lastName")
+        String lastName
+){
+}
