515c79db18672ef125eba667476b7046ae9bbf9c
Shogo Miura <138654083+ShogoMiura@users.noreply.github.com>
2024-01-28 17:38:15 +0900

    EmployeeRepositoryImpl仮実装。

---
+++ src/main/java/com/examination2/miura/infrastructure/repository/EmployeeRepositoryImpl.java
@@ -0,0 +1,13 @@
+package com.examination2.miura.infrastructure.repository;
+
+import com.examination2.miura.domain.Employee;
+import com.examination2.miura.domain.EmployeeRepository;
+
+import java.util.List;
+
+public class EmployeeRepositoryImpl implements EmployeeRepository {
+  @Override
+  public List<Employee> findAllEmployees() {
+    return null;
+  }
+}
