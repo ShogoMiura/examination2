555319c9e1b826adcef1a9efcbe9c442fd9ea13d
Shogo Miura <138654083+ShogoMiura@users.noreply.github.com>
2024-01-29 06:22:47 +0900

    EmployeeResponseクラスにofメソッドを仮実装。

--- src/main/java/com/examination2/miura/presentation/response/EmployeeResponse.java
+++ src/main/java/com/examination2/miura/presentation/response/EmployeeResponse.java
@@ -1,13 +1,14 @@
 package com.examination2.miura.presentation.response;
 
+import com.examination2.miura.domain.Employee;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 /**
  * 従業員情報のレスポンスを表すクラスです。
  *
- * @param id 従業員のID。
+ * @param id        従業員のID。
  * @param firstName 従業員の名前。
- * @param lastName 従業員の姓。
+ * @param lastName  従業員の姓。
  */
 public record EmployeeResponse(
         @JsonProperty("id")
@@ -16,5 +17,8 @@ public record EmployeeResponse(
         String firstName,
         @JsonProperty("lastName")
         String lastName
-){
+) {
+  public static EmployeeResponse of(Employee employee) {
+    return null;
+  }
 }
