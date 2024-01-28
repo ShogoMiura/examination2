116ce61cddc08fb2558a3ff8ae4338cae5ce0ddd
Shogo Miura <138654083+ShogoMiura@users.noreply.github.com>
2024-01-29 04:33:41 +0900

    EmployeeControllerに全件検索メソッド実装。

--- src/main/java/com/examination2/miura/presentation/EmployeeController.java
+++ src/main/java/com/examination2/miura/presentation/EmployeeController.java
@@ -1,18 +1,27 @@
 package com.examination2.miura.presentation;
 
+import com.examination2.miura.application.FindAllEmployeesUseCase;
+import com.examination2.miura.domain.Employee;
 import com.examination2.miura.presentation.response.AllEmployeesResponse;
+import com.examination2.miura.presentation.response.EmployeeResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * HTTPリクエストを処理するコントローラークラスです。
  */
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/")
 public class EmployeeController {
+  private final FindAllEmployeesUseCase findAll;
 
   /**
    * ルートエンドポイントへのHTTP GETリクエストを処理します。
@@ -23,7 +32,16 @@ public class EmployeeController {
     // do nothing
   }
 
+  @GetMapping("/v1/employees")
+  @ResponseStatus(HttpStatus.OK)
   public AllEmployeesResponse findALlEmployees() {
-    return null;
+    List<Employee> employeeList = findAll.execute();
+    List<EmployeeResponse> result = new ArrayList<>();
+    for (Employee employee : employeeList) {
+      result.add(new EmployeeResponse(employee.id(), employee.firstName(), employee.lastName()));
+    }
+
+    AllEmployeesResponse response = new AllEmployeesResponse(result);
+    return response;
   }
 }
