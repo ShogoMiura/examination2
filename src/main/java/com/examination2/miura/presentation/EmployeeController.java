99fe061d14161b11f854a1675c06644e239ad460
Shogo Miura <138654083+ShogoMiura@users.noreply.github.com>
2024-01-29 04:23:12 +0900

    EmployeeControllerに全件検索メソッドを仮実装。テスト実装。

--- src/main/java/com/examination2/miura/presentation/EmployeeController.java
+++ src/main/java/com/examination2/miura/presentation/EmployeeController.java
@@ -1,5 +1,6 @@
 package com.examination2.miura.presentation;
 
+import com.examination2.miura.presentation.response.AllEmployeesResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,5 +21,9 @@ public class EmployeeController {
   @ResponseStatus(HttpStatus.OK)
   public void getRoot() {
     // do nothing
+  }
+
+  public AllEmployeesResponse findALlEmployees() {
+    return null;
   }
 }
--- src/test/java/com/examination2/miura/presentation/EmployeeControllerTest.java
+++ src/test/java/com/examination2/miura/presentation/EmployeeControllerTest.java
@@ -1,13 +1,20 @@
 package com.examination2.miura.presentation;
 
+import com.examination2.miura.application.FindAllEmployeesUseCase;
+import com.examination2.miura.domain.Employee;
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.List;
+
 import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.when;
 
 
 @WebMvcTest
@@ -16,6 +23,9 @@ class EmployeeControllerTest {
   @Autowired
   MockMvc mockMvc;
 
+  @MockBean
+  FindAllEmployeesUseCase findAllEmployeesUseCase;
+
   @BeforeEach
   void setUp() {
     RestAssuredMockMvc.mockMvc(mockMvc);
@@ -23,10 +33,38 @@ class EmployeeControllerTest {
 
   @Test
   void ルートURLへのアクセスを受け付ける() {
+    // execute & assert
     given()
             .when()
             .get("/")
             .then()
             .statusCode(200);
+  }
+
+  @Test
+  void 全従業員の情報が正常に取得できる場合() {
+    // setup
+    when(findAllEmployeesUseCase.execute())
+            .thenReturn(
+                    List.of(
+                            new Employee("1", "Taro", "Yamada"),
+                            new Employee("2", "Jiro", "Yamada")
+                    )
+            );
+
+    // execute & assert
+    given()
+            .when()
+            .get("/v1/employees")
+            .then()
+            .statusCode(200)
+            .assertThat()
+            .body("employees.size()", is(2))
+            .body("employees[0].id", is("1"))
+            .body("employees[0].firstName", is("Taro"))
+            .body("employees[0].lastName", is("Yamada"))
+            .body("employees[1].id", is("2"))
+            .body("employees[1].firstName", is("Jiro"))
+            .body("employees[1].lastName", is("Yamada"));
   }
 }
