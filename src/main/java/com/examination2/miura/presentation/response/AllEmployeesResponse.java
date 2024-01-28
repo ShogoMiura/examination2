76ad2b00421874f856e0a7dbc3943a0cd1d91633
Shogo Miura <138654083+ShogoMiura@users.noreply.github.com>
2024-01-29 04:15:16 +0900

    AllEmployeesResponse実装。

---
+++ src/main/java/com/examination2/miura/presentation/response/AllEmployeesResponse.java
@@ -0,0 +1,15 @@
+package com.examination2.miura.presentation.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.List;
+
+/**
+ * 全ての従業員のレスポンスを表すクラスです。
+ *
+ * @param allEmployeesResponse EmployeeResponse オブジェクトのリスト。
+ */
+public record AllEmployeesResponse(
+        @JsonProperty("employees")
+        List<EmployeeResponse> allEmployeesResponse
+) {
+}
