a155f24be5bced9f4dab977ea79ff5369ca3659c
Shogo Miura <138654083+ShogoMiura@users.noreply.github.com>
2024-01-29 03:11:36 +0900

    EmployeeRepositoryImpl実装。

--- src/main/java/com/examination2/miura/infrastructure/repository/EmployeeRepositoryImpl.java
+++ src/main/java/com/examination2/miura/infrastructure/repository/EmployeeRepositoryImpl.java
@@ -2,12 +2,27 @@ package com.examination2.miura.infrastructure.repository;
 
 import com.examination2.miura.domain.Employee;
 import com.examination2.miura.domain.EmployeeRepository;
+import com.examination2.miura.infrastructure.entity.EmployeeEntity;
+import com.examination2.miura.infrastructure.mapper.EmployeeMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class EmployeeRepositoryImpl implements EmployeeRepository {
+
+  @Autowired
+  private EmployeeMapper mapper;
+
   @Override
   public List<Employee> findAllEmployees() {
-    return null;
+    List<EmployeeEntity> employeeEntities = mapper.findAllEmployees();
+
+    List<Employee> result = new ArrayList<>();
+    for (EmployeeEntity entity : employeeEntities) {
+      result.add(new Employee(entity.id(), entity.firstName(), entity.lastName()));
+    }
+
+    return result;
   }
 }
