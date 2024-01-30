c3a17b0d1f5f56bfb5f5b3cf3496efd6ad2da101
Shogo Miura <138654083+ShogoMiura@users.noreply.github.com>
2024-01-31 02:08:22 +0900

    CreateEmployeeUseCase実装。

--- src/main/java/com/examination2/miura/application/CreateEmployeeUseCase.java
+++ src/main/java/com/examination2/miura/application/CreateEmployeeUseCase.java
@@ -2,10 +2,33 @@ package com.examination2.miura.application;
 
 import com.examination2.miura.application.dto.CreateEmployeeDto;
 import com.examination2.miura.domain.Employee;
+import com.examination2.miura.domain.EmployeeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+/**
+ * CreateEmployeeUseCase は従業員の新規登録を行います。
+ */
+@RequiredArgsConstructor
+@Service
 public class CreateEmployeeUseCase {
+  private final EmployeeRepository repository;
 
+  /**
+   * 従業員の新規登録を実行します。
+   *
+   * @param dto 新規登録する従業員情報。
+   * @return 登録されたEmployee オブジェクト。
+   */
+  @Transactional
   public Employee execute(CreateEmployeeDto dto) {
-    return null;
+    return repository.createEmployee(
+            new Employee(
+                    repository.getNextEmployeeId().toString(),
+                    dto.firstName(),
+                    dto.lastName()
+            )
+    );
   }
 }
