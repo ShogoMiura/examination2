624f336c2dab522013317d2ba178c416a3ad3027
Shogo Miura <138654083+ShogoMiura@users.noreply.github.com>
2024-01-30 22:12:32 +0900

    ErrorResponseクラス実装。 GlobalExceptionHandlerクラス仮実装。

---
+++ src/main/java/com/examination2/miura/common/GlobalExceptionHandler.java
@@ -0,0 +1,5 @@
+package com.examination2.miura.common;
+
+public class GlobalExceptionHandler extends RuntimeException {
+
+}
---
+++ src/main/java/com/examination2/miura/presentation/response/ErrorResponse.java
@@ -0,0 +1,21 @@
+package com.examination2.miura.presentation.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.List;
+
+/**
+ * エラーレスポンスを表現するレコードクラスです。
+ *
+ * @param code エラーコード。
+ * @param message エラーメッセージ。
+ * @param details エラーの詳細。
+ */
+public record ErrorResponse(
+        @JsonProperty("code")
+        String code,
+        @JsonProperty("message")
+        String message,
+        @JsonProperty("details")
+        List<String> details
+) {
+}
