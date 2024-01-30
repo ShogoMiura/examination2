57581ca9a4970310a730627d7ee08ffb571134fc
Shogo Miura <138654083+ShogoMiura@users.noreply.github.com>
2024-01-30 22:08:12 +0900

    EmployeeNotFoundExceptionクラス実装。

---
+++ src/main/java/com/examination2/miura/application/exception/EmployeeNotFoundException.java
@@ -0,0 +1,21 @@
+package com.examination2.miura.application.exception;
+
+import lombok.Getter;
+
+/**
+ * 従業員が見つからない場合にスローされる例外クラスです。
+ */
+@Getter
+public class EmployeeNotFoundException extends RuntimeException {
+  private final String id;
+
+  /**
+   * 指定された従業員IDで新しい EmployeeNotFoundExceptionを構築します。
+   *
+   * @param id 例外が発生した従業員のID
+   */
+  public EmployeeNotFoundException(String id) {
+    super();
+    this.id = id;
+  }
+}
