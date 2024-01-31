1f898a78a11d84ad2fcc472283ac115d84118958
Shogo Miura <138654083+ShogoMiura@users.noreply.github.com>
2024-01-31 19:07:38 +0900

    EmployeeControllerにおいて、createEmployeeメソッド実装。

--- src/main/java/com/examination2/miura/presentation/EmployeeController.java
+++ src/main/java/com/examination2/miura/presentation/EmployeeController.java
@@ -3,18 +3,25 @@ package com.examination2.miura.presentation;
 import com.examination2.miura.application.CreateEmployeeUseCase;
 import com.examination2.miura.application.FindAllEmployeesUseCase;
 import com.examination2.miura.application.FindEmployeeByIdUseCase;
+import com.examination2.miura.application.dto.CreateEmployeeDto;
 import com.examination2.miura.domain.Employee;
 import com.examination2.miura.presentation.request.CreateEmployeeRequest;
 import com.examination2.miura.presentation.response.AllEmployeesResponse;
 import com.examination2.miura.presentation.response.EmployeeResponse;
+import java.net.URI;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
 
 /**
  * HTTPリクエストを処理するコントローラークラスです。
@@ -59,7 +66,25 @@ public class EmployeeController {
     return EmployeeResponse.of(employee);
   }
 
-  public ResponseEntity<Void> createEmployee(CreateEmployeeRequest request) {
-    return null;
+  /**
+   * 従業員を新規登録します。
+   *
+   * @param request 作成する従業員の情報を含むリクエストデータ
+   * @return 作成された従業員のリソースへのURIとともに、HTTPステータス201を返します。
+   */
+  @PostMapping("/v1/employees")
+  @ResponseStatus(HttpStatus.CREATED)
+  public ResponseEntity<Void> createEmployee(@RequestBody @Validated CreateEmployeeRequest request) {
+    Employee employee = createEmployeeUseCase.execute(
+            new CreateEmployeeDto(request.firstName(), request.lastName())
+    );
+
+    URI uri = UriComponentsBuilder
+            .fromUriString(ServletUriComponentsBuilder.fromCurrentRequestUri().toUriString())
+            .path("/" + employee.id())
+            .build()
+            .toUri();
+
+    return ResponseEntity.created(uri).build();
   }
 }
