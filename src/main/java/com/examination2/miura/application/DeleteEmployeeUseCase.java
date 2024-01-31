3f8d4416984dd7503739faea935fd48e7edee19c
Shogo Miura <138654083+ShogoMiura@users.noreply.github.com>
2024-02-01 00:29:42 +0900

    DeleteEmployeeUseCase実装。

--- src/main/java/com/examination2/miura/application/DeleteEmployeeUseCase.java
+++ src/main/java/com/examination2/miura/application/DeleteEmployeeUseCase.java
@@ -1,18 +1,29 @@
 package com.examination2.miura.application;
 
 import com.examination2.miura.application.exception.EmployeeNotFoundException;
+import com.examination2.miura.domain.EmployeeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * 指定したIDの従業員情報を削除するためのユースケースクラスです。
  */
+@RequiredArgsConstructor
+@Service
 public class DeleteEmployeeUseCase {
+  private final EmployeeRepository repository;
+
   /**
    * 指定したIDの従業員情報を取得します。
    *
    * @param id 削除する従業員のID。
    * @throws EmployeeNotFoundException 指定された従業員IDに対するデータが見つからない場合にスローされます。
    */
+  @Transactional
   public void execute(String id) {
+    if (repository.findEmployeeById(id).isEmpty()) throw new EmployeeNotFoundException(id);
 
+    repository.deleteEmployee(id);
   }
 }
